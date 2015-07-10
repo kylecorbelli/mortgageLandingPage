66955ef7a816730667cd8cc4fc09081b4cbf5380
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-09 19:53:59 -0700

    Added base HTML & PHP

--- public_html/default.php
+++ public_html/default.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html>
+    <?PHP
+        include 'include/head.html';
+    ?>
+    <body>
+        <div id='page-wrapper'></div>
+        <?PHP
+            include 'include/footer.php';
+        ?>
+    </body>
+</html>
