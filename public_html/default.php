545c09dc03a7200472c17c10c9ecac97ea3285fe
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-10 12:27:04 -0700

    Update default.php
    Added additional inputs: 'Property Type' and 'Occupancy'

--- public_html/default.php
+++ public_html/default.php
@@ -15,28 +15,52 @@
                             <div class='row checkbox'>
                                 <div class='col-sm-6'>
                                     <label>
-                                        <input name='refinanceReason' type='checkbox' /> Lower Monthly Payment
+                                        <input name='refinanceReason' type='checkbox' value='Lower-Monthly-Payment' /> Lower Monthly Payment
                                     </label>
                                 </div>
                                 <div class='col-sm-6'>
                                     <label>
-                                        <input name='refinanceReason' type='checkbox' /> Debt Consolidation
+                                        <input name='refinanceReason' type='checkbox' value='Debt-Consolidation' /> Debt Consolidation
                                     </label>
                                 </div>
                             </div>
                             <div class='row checkbox'>
                                 <div class='col-sm-6'>
                                     <label>
-                                        <input name='refinanceReason' type='checkbox' /> Shorten Loan Term
+                                        <input name='refinanceReason' type='checkbox' value='Shorten-Loan-Term' /> Shorten Loan Term
                                     </label>
                                 </div>
                                 <div class='col-sm-6'>
                                     <label>
-                                        <input name='refinanceReason' type='checkbox' /> Cash Out Refinance
+                                        <input name='refinanceReason' type='checkbox' value='Cash-Out-Refinance' /> Cash Out Refinance
                                     </label>
                                 </div>
                             </div>
                         </div> <!-- END Mortgage Goal Input -->
+                        <div> <!-- Property Type Input -->
+                            <h2>Property Type:</h2>
+                            <div class='row'>
+                                <select class='form-control'>
+                                    <option hidden disable selected>-Select One-</option>
+                                    <option value='Single-Family-Detached-House'>Single-Family Detached House</option>
+                                    <option value='Condominium'>Condominium</option>
+                                    <option value='Townhouse'>Townhouse</option>
+                                    <option value='Multi-Family-Building'>Multi-Family Building</option>
+                                    <option value='Land-Lot'>Land / Lot</option>
+                                </select>
+                            </div>
+                        </div> <!-- END Property Type Input -->
+                        <div> <!-- Occupancy Input -->
+                            <h2>Occupancy Type:</h2>
+                            <div class='row'>
+                                <select class='form-control'>
+                                    <option hidden disable selected>-Select One-</option>
+                                    <option value='Owner-Occupied'>Owner-Occupied</option>
+                                    <option value='Vacation-Second-Home'>Vacation / Second Home</option>
+                                    <option value='Income-Investment-Property'>Income / Investment Property</option>
+                                </select>
+                            </div>
+                        </div> <!-- END Occupancy Input -->
                     </div> <!-- END Well that holds all inputs -->
                 </div>
             </div>
