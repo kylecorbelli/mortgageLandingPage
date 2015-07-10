04516a7d69c885fa9e9be27c26d38a0fcccb19a9
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-09 19:55:49 -0700

    Create head.php
    This is the head HTML that will be included via PHP into all pages in this project.

---
+++ include/head.php
@@ -0,0 +1,10 @@
+<head>
+    <title>Refinace | Great Rates</title>
+    <meta charset='utf-8' />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js' type='text/javascript' defer></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" defer></script>
+    <link rel='stylesheet' type='text/css' href='' />
+    <script src='' type='text/javascript' defer></script>
+</head>
