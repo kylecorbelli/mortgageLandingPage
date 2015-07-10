3d9402a216858fb9a694bf143558b119d33f52c7
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-09 20:16:31 -0700

    Cleaned up CSS & JS paths

--- public_html/include/head.php
+++ public_html/include/head.php
@@ -5,6 +5,6 @@
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js' type='text/javascript' defer></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" defer></script>
-    <link rel='stylesheet' type='text/css' href='' />
-    <script src='' type='text/javascript' defer></script>
+    <link rel='stylesheet' type='text/css' href='css/style.css' />
+    <script src='js/javascript.js' type='text/javascript' defer></script>
 </head>
