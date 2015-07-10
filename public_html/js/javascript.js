033a8e9752fd23b8e0d6ce375f0a1995df325c3b
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-10 16:55:41 -0700

    Update javascript.js
    Added script to format user's numerical input with commas to separate the thousands.

--- public_html/js/javascript.js
+++ public_html/js/javascript.js
@@ -1,7 +1,11 @@
 $(function() {
 
+// Formats the user's numerical input with commas separating the thousands while they're typing it in.
 $('.numberToComma').on('keyup', function() {
-  alert($(this).val());
+  var unformattedText = this.value; // Selects the input that triggered the event listener.
+  noCommaText = unformattedText.replace(/,/g, ''); // Strips out all existing commas.
+  formattedText = noCommaText.replace(/\B(?=(\d{3})+(?!\d))/g, ","); // Adds commas when numerical characters are followed by exactly three consecutive numerical characters.
+  this.value = formattedText; // Replaces the input field's value with the newly-formatted string.
 });
 
 });
