88f29b899df6babf29eac94dfd9d7bf4b7be30e6
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-10 15:46:32 -0700

    Update default.php
    Added 'input-group' classes for aesthetic appeal.

--- public_html/default.php
+++ public_html/default.php
@@ -46,49 +46,49 @@
                                 </div>
                             </div>
                         </div> <!-- END Mortgage Goal Input -->
-                        <div> <!-- Property Type Input -->
-                            <h2>Property Type:</h2>
-                            <div class='row'>
-                                <select class='form-control'>
-                                    <option hidden disable selected>-Select One-</option>
-                                    <option value='Single-Family-Detached-House'>Single-Family Detached House</option>
-                                    <option value='Condominium'>Condominium</option>
-                                    <option value='Townhouse'>Townhouse</option>
-                                    <option value='Multi-Family-Building'>Multi-Family Building</option>
-                                    <option value='Land-Lot'>Land / Lot</option>
-                                </select>
-                            </div>
+                        <div class='form-group'> <!-- Property Type Input -->
+                            <label for='Property-Type'><h2>Property Type:</h2></label>
+                            <select id='Property-Type' class='form-control' name='Property-Type'>
+                                <option hidden disable selected>-Select One-</option>
+                                <option value='Single-Family-Detached-House'>Single-Family Detached House</option>
+                                <option value='Condominium'>Condominium</option>
+                                <option value='Townhouse'>Townhouse</option>
+                                <option value='Multi-Family-Building'>Multi-Family Building</option>
+                                <option value='Land-Lot'>Land / Lot</option>
+                            </select>
                         </div> <!-- END Property Type Input -->
-                        <div> <!-- Occupancy Input -->
-                            <h2>Occupancy Type:</h2>
-                            <div class='row'>
-                                <select class='form-control'>
-                                    <option hidden disable selected>-Select One-</option>
-                                    <option value='Owner-Occupied'>Owner-Occupied</option>
-                                    <option value='Vacation-Second-Home'>Vacation / Second Home</option>
-                                    <option value='Income-Investment-Property'>Income / Investment Property</option>
-                                </select>
-                            </div>
+                        <div class='form-group'> <!-- Occupancy Input -->
+                            <label for='Occupancy-Type'><h2>Occupancy Type:</h2></label>
+                            <select id='Occupancy-Type' class='form-control' name='Occupancy-Type'>
+                                <option hidden disable selected>-Select One-</option>
+                                <option value='Owner-Occupied'>Owner-Occupied</option>
+                                <option value='Vacation-Second-Home'>Vacation / Second Home</option>
+                                <option value='Income-Investment-Property'>Income / Investment Property</option>
+                            </select>
                         </div> <!-- END Occupancy Input -->
                         <div class='form-group'> <!-- Property Value Input -->
                             <label for='Property-Value'><h2>Estimated Property Value:</h2></label>
-                            <input id='Property-Value' class='form-control' type='number' name='Property-Value'/>
+                            <div class='input-group'>
+                                <span class='input-group-addon'>$</span>
+                                <input id='Property-Value' class='form-control' type='number' name='Property-Value'/>
+                            </div>
                         </div> <!-- END Property Value Input -->
                         <div class='form-group'> <!-- Mortgage Balance Input -->
                             <label for='Loan-Balance'><h2>Approximate Mortgage Balance:</h2></label>
-                            <input class='form-control' type='number' id='Loan-Balance' name='Loan-Balance'/>
+                            <div class='input-group'>
+                                <span class='input-group-addon'>$</span>
+                                <input id='Loan-Balance' class='form-control' type='number' name='Loan-Balance'/>
+                            </div>
                         </div> <!-- END Mortgage Balance Input -->
                         <div class='form-group'> <!-- Cash Out Input -->
                             <label for='Cash-Out'><h2>Desired Cash Out</h2></label>
                             <input id='Cash-Out' class='form-control' type='number' name='Cash-Out'/>
                         </div> <!-- END Cash Out Input -->
                         <div class='form-group'> <!-- Current Rate Input -->
-                            <label>
-                                <h2>First Mortgage Interest Rate</h2>
-                                <input class='form-control' type='number' name='First-Mortgage-Rate'/>
-                            </label>
+                            <label for='First-Mortgage-Rate'><h2>First Mortgage Interest Rate</h2></label>
+                            <input id='First-Mortgage-Rate' class='form-control' type='number' name='First-Mortgage-Rate'/>
                         </div> <!-- ENDCurrent Rate Input -->
-                        <div> <!-- VA Eligibility Input -->
+                        <div class='form-group'> <!-- VA Eligibility Input -->
                             <h2>Are You a Veteran?</h2>
                             <div class='form-group'>
                                 <div class='radio'>
@@ -103,42 +103,32 @@
                                 </div>
                             </div>
                         </div> <!-- END VA Eligibility Input -->
-                        <div> <!-- Credit Input -->
-                            <h2>How is Your Credit?</h2>
-                            <div class='row'>
-                                <select class='form-control'>
-                                    <option hidden disable selected>-Select One-</option>
-                                    <option value='Excellent-720-or-Above'>Excellent (720 or Above)</option>
-                                    <option value='Good-660-719'>Good (660 - 719)</option>
-                                    <option value='Average-620-659'>Average (620 - 659)</option>
-                                    <option value='Below-Average-580-619'>Below Average (580 - 619)</option>
-                                    <option value='Poor-579-and-Below'>Poor (579 and Below)</option>
-                                </select>
-                            </div>
+                        <div class='form-group'> <!-- Credit Input -->
+                            <label for='Credit'><h2>How is Your Credit?</h2></label>
+                            <select id='Credit' class='form-control' name='Credit'>
+                                <option hidden disable selected>-Select One-</option>
+                                <option value='Excellent-720-or-Above'>Excellent (720 or Above)</option>
+                                <option value='Good-660-719'>Good (660 - 719)</option>
+                                <option value='Average-620-659'>Average (620 - 659)</option>
+                                <option value='Below-Average-580-619'>Below Average (580 - 619)</option>
+                                <option value='Poor-579-and-Below'>Poor (579 and Below)</option>
+                            </select>
                         </div> <!-- END Credit Input -->
-                        <div> <!-- First Name Input -->
-                            <label>
-                                <h2>First Name:</h2>
-                                <input class='form-control' type='text' name='First-Name' />
-                            </label>
+                        <div class='form-group'> <!-- First Name Input -->
+                            <label for='First-Name'><h2>First Name:</h2></label>
+                            <input id='First-Name' class='form-control' type='text' name='First-Name' />
                         </div> <!-- END First Name Input -->
-                        <div> <!-- Last Name Input -->
-                            <label>
-                                <h2>Last Name:</h2>
-                                <input class='form-control' type='text' name='Last-Name' />
-                            </label>
+                        <div class='form-group'> <!-- Last Name Input -->
+                            <label for='Last-Name'><h2>Last Name:</h2></label>
+                            <input id='Last-Name' class='form-control' type='text' name='Last-Name' />
                         </div> <!-- END Last Name Input -->
-                        <div> <!-- Email Input -->
-                            <label>
-                                <h2>Email:</h2>
-                                <input class='form-control' type='email' name='Email' />
-                            </label>
+                        <div class='form-group'> <!-- Email Input -->
+                            <label for='Email'><h2>Email:</h2></label>
+                            <input id='Email' class='form-control' type='email' name='Email' />
                         </div> <!-- END Email Input -->
-                        <div> <!-- Phone Input -->
-                            <label>
-                                <h2>Phone:</h2>
-                                <input class='form-control' type='tel' name='Phone' />
-                            </label>
+                        <div class='form-group'> <!-- Phone Input -->
+                            <label for='Phone'><h2>Phone:</h2></label>
+                            <input id='Phone' class='form-control' type='tel' name='Phone' />
                         </div> <!-- END Phone Input -->
                     </div> <!-- END Well that holds all inputs -->
                 </div>
