407d87dce5dbefba7b11923c10191096d3380220
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-10 11:01:19 -0700

    Update default.php
    Testing to see if MySQL is properly connected.

--- public_html/default.php
+++ public_html/default.php
@@ -2,13 +2,34 @@
 <html>
     <?PHP
         include 'include/head.php';
-        include 'include/connect.php';
+        include 'include/connection.php';
     ?>
     <body>
         <div id='page-wrapper' class='container-fluid'>
             <div class='row'>
                 <div class='col-med-12'>
-                    (Placeholder for a table)
+                    <?PHP
+                        $columnsQuery = "
+                            SHOW COLUMNS
+                            FROM `Leads`
+                        ";
+                        $entriesQuery = "
+                            SELECT *
+                            FROM `Leads`
+                            ORDER BY `ID`
+                        ";
+                        $columns = mysql_query($columnsQuery);
+                        $result = mysql_query($entriesQuery);
+                        echo "<table class='table table-striped'><thead><tr>";
+                        while($columnHead = mysql_fetch_array($columns)) {
+                            echo '<th>' . $columnHead['Field'] . '</th>';
+                        }
+                        echo '</tr></thead><tbody>';
+                        while($entry = mysql_fetch_array($result)) {
+                            echo '<tr><td>' . $entry['ID'] . '</td><td>' . $entry['Date Added'] . '</td><td>' . $entry['Zip Code'] . '</td><td>' . $entry['Estimated Credit'] . '</td><td>' . $entry['Property Type'] . '</td><td>' . $entry['Estimated Property Value'] . '</td><td>' . $entry['Approximate Mortgage Balance'] . '</td><td>' . $entry['Current Mortgage Rate'] . '</td><td>' . $entry['Reason for Refinance'] . '</td><td>' . $entry['VA Eligibly'] . '</td><td>' . $entry['First Name'] . '</td><td>' . $entry['Last Name'] . '</td><td>' . $entry['Email'] . '</td><td>' . $entry['Phone'] . '</td><td>' . $entry['Subject Address'] . '</td></tr>';
+                        }
+                        echo '</tbody></table>';
+                    ?>
                 </div>
             </div>
         </div>
