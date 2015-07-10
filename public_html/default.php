e8c17b5b246f36c97935379ee829f7366b1deadf
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-10 10:16:12 -0700

    Update default.php

--- public_html/default.php
+++ public_html/default.php
@@ -2,9 +2,16 @@
 <html>
     <?PHP
         include 'include/head.php';
+        include 'include/connect.php';
     ?>
     <body>
-        <div id='page-wrapper'></div>
+        <div id='page-wrapper' class='container-fluid'>
+            <div class='row'>
+                <div class='col-med-12'>
+                    (Placeholder for a table)
+                </div>
+            </div>
+        </div>
         <?PHP
             include 'include/footer.php';
         ?>
