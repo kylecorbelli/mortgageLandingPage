e4583e1845bb30382d38080fe5c22f8dbe1a96b3
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-09 19:57:35 -0700

    Create footer.php
    This is the footer file that will be included via PHP into all pages on this site.

---
+++ include/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <?PHP
+        if(date('Y') == 2015) {
+            echo 'Copyright &copy; ' . date('Y') . ' Kyle Corbelli. All rights reserved';
+        } else {
+            echo 'Copyright &copy; 2015 - ' . date('Y') . ' Kyle Corbelli. All rights reserved';
+        }
+    ?>
+</footer>
