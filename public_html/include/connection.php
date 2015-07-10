e67b1cf04e308c1908c0f475d66298681a239687
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-10 06:51:19 -0700

    Create connection.php
    This file establishes the connection to the MySQL database. The password here is incorrect for security.

---
+++ public_html/include/connection.php
@@ -0,0 +1,9 @@
+<?PHP
+    $dbHost = 'mysql6.000webhost.com';
+    $db = 'a2997383_leads';
+    $dbUser = 'a2997383_kyle';
+    $dbPassword = '';
+	
+    $conn = mysql_connect($dbHost, $dbUser, $dbPassword);
+    mysql_select_db($db);
+?>
