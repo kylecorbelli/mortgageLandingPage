f949082046baa5597e0058687a77ee76a388d474
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-12 10:24:26 -0700

    Update javascript.js
    Edited the thousands-comma function to trigger a change to the input.

--- public_html/js/javascript.js
+++ public_html/js/javascript.js
@@ -2,10 +2,10 @@ $(function() {
 
 // Formats the user's numerical input with commas separating the thousands while they're typing it in.
 $('.numberToComma').on('keyup', function() {
-  var unformattedText = this.value; // Selects the input that triggered the event listener.
+  var unformattedText = $(this).val(); // Selects the input that triggered the event listener.
   noCommaText = unformattedText.replace(/,/g, ''); // Strips out all existing commas.
   formattedText = noCommaText.replace(/\B(?=(\d{3})+(?!\d))/g, ","); // Adds commas when numerical characters are followed by exactly three consecutive numerical characters.
-  this.value = formattedText; // Replaces the input field's value with the newly-formatted string.
+  $(this).val(formattedText).trigger('change'); // Replaces the input field's value with the newly-formatted string.
 });
 
 // Adds error styling if the user has focused on and then skipped a required input
