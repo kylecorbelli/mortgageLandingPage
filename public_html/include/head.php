d11f4214e7afc4efe186aca0bd783b5d78ad812c
pacificLiving <pacificLiving@users.noreply.github.com>
2015-07-14 12:25:48 -0700

    Update head.php

--- public_html/include/head.php
+++ public_html/include/head.php
@@ -2,6 +2,7 @@
     <title>Refinace | Great Rates</title>
     <meta charset='utf-8' />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='shortcut icon' href='images/idealIcon.ico' />
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js' type='text/javascript' defer></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" defer></script>
